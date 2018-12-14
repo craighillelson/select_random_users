c8a2eed0d715bffae7e93a1d7b6a2c7fedb2986e
Craig <craig.hillelson@intersection.com>
2018-12-14 18:03:33 -0500

    Initial commit

---
+++ select_random_users.py
@@ -0,0 +1,10 @@
+import csv
+import random
+
+# prompt user for how many random users they would like to review
+# prompt_user = raw_input("How many random users would you like selected?")
+# import a csv and throw it into a list
+# get the length of the list
+# number_of_users = len(lst)
+# for the number of users specified by the user, select a random user, throw 
+# that user into a list random_user_lst and remove the same user from lst
