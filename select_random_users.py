dd4e4100e77a878c7fcf64770aa7908b006366c7
Craig <craig.hillelson@intersection.com>
2018-12-18 18:20:25 -0500

    import csv

--- select_random_users.py
+++ select_random_users.py
@@ -2,9 +2,22 @@ import csv
 import random
 
 # prompt user for how many random users they would like to review
-# prompt_user = raw_input("How many random users would you like selected?")
+number_of_users = raw_input("How many random users would you like selected? ")
+# print(number_of_users)
+
+# create list
+users = []
+
 # import a csv and throw it into a list
+with open('OktaPasswordHealth.csv') as f:
+	f_csv = csv.DictReader(f)
+	for row in f_csv:
+		users.append(row)
+
 # get the length of the list
+users_qty = len(users)
+print(users_qty)
+
 # number_of_users = len(lst)
 # for the number of users specified by the user, select a random user, throw 
 # that user into a list random_user_lst and remove the same user from lst
