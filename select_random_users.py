7e73a6894aa3f9ef096c2bfe81e19d890a30599a
Craig Hillelson <craig.hillelson@intersection.com>
2018-12-26 09:02:02 -0500

    refactored based on pylint findings

--- select_random_users.py
+++ select_random_users.py
@@ -1,38 +1,39 @@
+""" __doc__ """
+
 # imports
 from datetime import date
 import csv
 import random
 
 # define today
-today = date.today()
+TODAY = date.today()
 
 # prompt user for how many random users they would like to review
-number_of_users = int(raw_input
-	("How many random users would you like to audit? "))
+NUMBER_OF_USERS = int(raw_input("How many random users would you like to audit? "))
 
 # create list to be populated
-users = []
+USERS = []
 
 # open csv and throw contents into a list
-with open('OktaPasswordHealth.csv') as csvfile:  
-		reader = csv.DictReader(csvfile)
-		for row in reader:
-			user = row['Login']
-			users.append(user)
-	
+with open('OktaPasswordHealth.csv') as csvfile:
+    READER = csv.DictReader(csvfile)
+    for row in READER:
+        user = row['Login']
+        USERS.append(user)
+
 # get the length of the list
-users_qty = int(len(users))
+USERS_QTY = int(len(USERS))
 
 # create a list of random numbers within a range from 0 to the number of users
 # equal to the number specified by the user
-random_numbers = random.sample(range(0, users_qty), number_of_users)
+RANDOM_NUMBERS = random.sample(range(0, USERS_QTY), NUMBER_OF_USERS)
 
 # write results to file
 with open('users_to_audit.txt', 'a') as text_file:
-    for i in random_numbers:
-    	user = str(users[i])
+    for i in RANDOM_NUMBERS:
+        user = str(USERS[i])
         text_file.write(user)
         text_file.write('\n')
 
 # update user
-print("'%s_users_to_audit.txt' exported successfully") % (today)
+print("'%s_users_to_audit.txt' exported successfully") % (TODAY)
