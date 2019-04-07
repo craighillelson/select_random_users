4c94e5383ca9d5ac759e5edf0d7c8599b4a1bffa
craighillelson <craighillelson@gmail.com>
2019-04-07 09:45:45 -0400

    renamed variables

--- select_random_users.py
+++ select_random_users.py
@@ -15,26 +15,24 @@ NUMBER_OF_USERS = int(input("How many random users would you like to audit? "))
 USERS = []
 
 # open csv and throw contents into a list
-with open('OktaPasswordHealth.csv') as csvfile:
+with open("OktaPasswordHealth.csv") as csvfile:
     READER = csv.DictReader(csvfile)
     for row in READER:
-        USERS.append(row['Login'])
-
+        USERS.append(row["Login"])
 
 # get the length of the list
 USERS_QTY = int(len(USERS))
 
 # create a list of random numbers within a range from 0 to the number of users
 # equal to the number specified by the user
-RANDOM_NUMBERS = random.sample(range(0, USERS_QTY), NUMBER_OF_USERS)
+RANDOM_INTEGERS = random.sample(range(0, USERS_QTY), NUMBER_OF_USERS)
 
 # write results to file
 USERS_TO_AUDIT = f"{TODAY}_users_to_audit.txt"
-with open(USERS_TO_AUDIT, 'w') as text_file:
-    for i in RANDOM_NUMBERS:
-        user = str(USERS[i])
-        text_file.write(user)
-        text_file.write('\n')
+with open(USERS_TO_AUDIT, "w") as text_file:
+    for integer in RANDOM_INTEGERS:
+        user = str(USERS[integer])
+        text_file.write(f"{user}\n")
 
 # update user
 print(f"{USERS_TO_AUDIT} exported successfully")
