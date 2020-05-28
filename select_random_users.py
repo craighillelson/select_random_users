7574fbc7688ac62bfcce55f6e6ccc3e4da3c8490
Craig Hillelson <craighillelson@gmail.com>
2020-05-28 07:45:52 -0400

    update select_random_users.py
    add doc strings
    made code more modular

--- select_random_users.py
+++ select_random_users.py
@@ -1,38 +1,43 @@
-""" __doc__ """
+"""Select random users from an OktaPasswordHealth csv for auditing purposes."""
 
-# imports
 import csv
 import random
 from datetime import date
+import pyinputplus as pyip
 
-# define today
-TODAY = date.today()
 
-# prompt user for how many random users they would like to review
-NUMBER_OF_USERS = int(input("How many random users would you like to audit? "))
+def populate_lst():
+    """Import 'OktaPasswordHealth.csv' and populate a list of logins."""
+    lst = []
+    with open('OktaPasswordHealth.csv') as csvfile:
+        READER = csv.DictReader(csvfile)
+        for row in READER:
+            lst.append(row['Login'])
 
-# create list to be populated
-USERS = []
+    return lst
 
-# open csv and throw contents into a list
-with open("OktaPasswordHealth.csv") as csvfile:
-    READER = csv.DictReader(csvfile)
-    for row in READER:
-        USERS.append(row["Login"])
 
-# get the length of the list
-USERS_QTY = int(len(USERS))
+def prompt_user():
+    """Prompt user for the number of users they'd like to audit."""
+    print('\nHow many random users would you like to audit?')
+    a = pyip.inputInt('> ', max=TOTAL_USERS)
+    return a
 
-# create a list of random numbers within a range from 0 to the number of users
-# equal to the number specified by the user
-RANDOM_INTEGERS = random.sample(range(0, USERS_QTY), NUMBER_OF_USERS)
 
-# write results to file
-USERS_TO_AUDIT = f"{TODAY}_users_to_audit.txt"
-with open(USERS_TO_AUDIT, "w") as text_file:
-    for integer in RANDOM_INTEGERS:
-        user = str(USERS[integer])
-        text_file.write(f"{user}\n")
+def write_to_file():
+    """Write to file and update user."""
+    TODAY = date.today()
+    USERS_TO_AUDIT = f'{TODAY}_users_to_audit.txt'
+    RANDOM_INTEGERS = random.sample(range(0, TOTAL_USERS), NUMBER_OF_USERS)
+    with open(USERS_TO_AUDIT, 'w') as text_file:
+        for integer in RANDOM_INTEGERS:
+            user = str(USERS[integer])
+            text_file.write(f'{user}\n')
 
-# update user
-print(f"{USERS_TO_AUDIT} exported successfully")
+    print(f'{USERS_TO_AUDIT} exported successfully\n')
+
+
+USERS = populate_lst()
+TOTAL_USERS = len(USERS)
+NUMBER_OF_USERS = prompt_user()
+write_to_file()
